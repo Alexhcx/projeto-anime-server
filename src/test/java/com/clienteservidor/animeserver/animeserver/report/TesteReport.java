eb5dd535362a39e3c8f7023d2b45a4708d5cb496
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2024-12-08 03:46:22 -0300

    - Dump do teste para o relatorio do Jasper

--- src/test/java/com/clienteservidor/animeserver/animeserver/report/TesteReport.java
+++ src/test/java/com/clienteservidor/animeserver/animeserver/report/TesteReport.java
@@ -1,40 +1,40 @@
-package com.clienteservidor.animeserver.animeserver.report;
+// package com.clienteservidor.animeserver.animeserver.report;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+// import java.io.File;
+// import java.util.HashMap;
+// import java.util.Map;
 
-import org.checkerframework.checker.units.qual.s;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.checkerframework.checker.units.qual.s;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.mock.web.MockHttpServletRequest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-import com.clienteservidor.animeserver.animeserver.AnimeserverApplication;
-import com.clienteservidor.animeserver.animeserver.ReportUtils;
+// import com.clienteservidor.animeserver.animeserver.AnimeserverApplication;
+// import com.clienteservidor.animeserver.animeserver.ReportUtils;
 
-import jakarta.servlet.ServletContext;
+// import jakarta.servlet.ServletContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = AnimeserverApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class TesteReport {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest(classes = AnimeserverApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+// public class TesteReport {
 
-  @Autowired
-  private ReportUtils reportUtils;
+//   @Autowired
+//   private ReportUtils reportUtils;
 
-  @Test
-  public void testImpressaoUsuario() throws Exception {
+//   @Test
+//   public void testImpressaoUsuario() throws Exception {
 
-    HashMap params = new HashMap();
-    // params.put("\\", 1);
-    MockHttpServletRequest request = new MockHttpServletRequest();
+//     HashMap params = new HashMap();
+//     // params.put("\\", 1);
+//     MockHttpServletRequest request = new MockHttpServletRequest();
 
-    String local = reportUtils.gerarRelatorioStringPath(params, request.getServletContext(),
-        "Relatorio-usuarios-modelo");
+//     String local = reportUtils.gerarRelatorioStringPath(params, request.getServletContext(),
+//         "Relatorio-usuarios-modelo");
 
-        System.out.println(local);
+//         System.out.println(local);
 
-  }
-}
+//   }
+// }
